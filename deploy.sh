00188c744ee135517706dd528650ffab3a057461
robinheinrich <robin.heinrich@mac.com>
2026-06-10 15:53:36 +0200

    Deploy Script angepasst und ausgebessert um die Daten direkt von git zu holen und zu installieren

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-cd /var/www/nextjs
-git pull https://github.com/robinheinrich/houseghost.de
+set -e
+
+echo "Starte Deployment..."
+
+cd /var/www/nextjs/houseghost.de
+
+if [ ! -d ".git" ]; then
+    echo "Klone Repository von GitHub..."
+    cd /var/www/nextjs
+    rm -rf houseghost.de
+    git clone https://github.com/robinheinrich/houseghost.de.git houseghost.de
+    cd houseghost.de
+else
+    echo "Aktualisiere Repository von GitHub..."
+    git fetch origin
+    git reset --hard origin/main
+fi
+
+echo "Installiere Dependencies..."
 npm install
+
+echo "Baue Projekt..."
 npm run build
-pm2 restart 
-echo "Deployment abgeschlossen!"
+
+echo "Starte App neu..."
+pm2 restart nextjs
+
+echo "Deployment erfolgreich!"
